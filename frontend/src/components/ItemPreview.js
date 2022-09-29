1dee23ef12c07cbeaf3c9523b0fdf41ec13ffe85
Ifeoluwah <spencerife.21@gmail.com>
2022-09-29 01:19:39 +0100

    fixed broken img

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,7 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image === "" ? "placeholder.png" : item.image}
+        src="placeholder.png"
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
